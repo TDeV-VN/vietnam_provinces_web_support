d008d0917d448e89201ae59a59aadade48c177a9
14h4i <pgkhai.dev@gmail.com>
2025-01-03 21:43:17 +0700

    feat: Optimize package library size.

--- lib/src/province_helper.dart
+++ lib/src/province_helper.dart
@@ -1,14 +1,26 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 import 'models/province.dart';
 
+/// Helper class for loading province data.
 class ProvinceHelper {
+  static const String _filePath =
+      'packages/vietnam_provinces/lib/assets/provinces.json.gz';
+
+  /// Loads the list of provinces from a compressed JSON file.
   static Future<List<Province>> loadProvinces() async {
-    final data = await rootBundle
-        .loadString('packages/vietnam_provinces/lib/assets/provinces.json');
-    final List<dynamic> jsonList = json.decode(data);
-    return jsonList.map((json) => Province.fromJson(json)).toList();
+    try {
+      final compressedData = await rootBundle.load(_filePath);
+      final jsonData =
+          utf8.decode(gzip.decode(compressedData.buffer.asUint8List()));
+      final List<dynamic> jsonList = jsonDecode(jsonData);
+
+      return jsonList.map((json) => Province.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception('Error loading provinces: $e');
+    }
   }
 }
