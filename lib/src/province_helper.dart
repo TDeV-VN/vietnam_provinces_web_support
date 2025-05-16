7cd05a72ab35951baf48497969bcefc543681a85
TDeV-VN <thanhtien.z8436@gmail.com>
2025-05-16 16:10:51 +0700

    .

--- lib/src/province_helper.dart
+++ lib/src/province_helper.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'dart:io';
+import 'package:archive/archive.dart';
 
 import 'package:flutter/services.dart';
 
@@ -14,8 +14,14 @@ class ProvinceHelper {
   static Future<List<Province>> loadProvinces() async {
     try {
       final compressedData = await rootBundle.load(_filePath);
-      final jsonData =
-          utf8.decode(gzip.decode(compressedData.buffer.asUint8List()));
+      // final jsonData =
+      //     utf8.decode(gzip.decode(compressedData.buffer.asUint8List()));
+      final List<int> gzippedBytes = bytes.buffer.asUint8List();
+      final List<int> decompressedBytes = GZipDecoder().decodeBytes(
+        gzippedBytes,
+        verify: true,
+      ); // Sử dụng GZipDecoder từ package archive
+      final jsonData = utf8.decode(decompressedBytes);
       final List<dynamic> jsonList = jsonDecode(jsonData);
 
       return jsonList.map((json) => Province.fromJson(json)).toList();
