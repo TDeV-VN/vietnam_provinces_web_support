656fe7c1c7d8e5b9fb75d31eff160993cc11b2ef
TDeV-VN <thanhtien.z8436@gmail.com>
2025-05-16 16:16:19 +0700

    .

--- lib/src/province_helper.dart
+++ lib/src/province_helper.dart
@@ -1,32 +1,29 @@
 import 'dart:convert';
 import 'package:archive/archive.dart';
-
 import 'package:flutter/services.dart';
-
 import 'models/province.dart';
 
-/// Helper class for loading province data.
 class ProvinceHelper {
   static const String _filePath =
       'packages/vietnam_provinces/lib/assets/provinces.json.gz';
 
-  /// Loads the list of provinces from a compressed JSON file.
   static Future<List<Province>> loadProvinces() async {
     try {
-      final compressedData = await rootBundle.load(_filePath);
-      // final jsonData =
-      //     utf8.decode(gzip.decode(compressedData.buffer.asUint8List()));
-      final List<int> gzippedBytes = bytes.buffer.asUint8List();
+      final ByteData compressedData = await rootBundle.load(_filePath);
+      final List<int> gzippedBytes = compressedData.buffer.asUint8List();
       final List<int> decompressedBytes = GZipDecoder().decodeBytes(
         gzippedBytes,
         verify: true,
-      ); // Sử dụng GZipDecoder từ package archive
-      final jsonData = utf8.decode(decompressedBytes);
-      final List<dynamic> jsonList = jsonDecode(jsonData);
-
-      return jsonList.map((json) => Province.fromJson(json)).toList();
+      );
+      final String jsonDataString = utf8.decode(decompressedBytes);
+      final List<dynamic> jsonList =
+          jsonDecode(jsonDataString) as List<dynamic>;
+      return jsonList
+          .map((json) => Province.fromJson(json as Map<String, dynamic>))
+          .toList();
     } catch (e) {
-      throw Exception('Error loading provinces: $e');
+      print('Error loading provinces: $e');
+      throw Exception('Failed to load provinces: $e');
     }
   }
 }
